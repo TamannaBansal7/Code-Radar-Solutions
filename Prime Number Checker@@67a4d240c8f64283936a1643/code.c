e290f38520c2c01f42273735ce6454805de1f702
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-02-10 19:13:22 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int n,fact_count;
+    printf("Input a positive number:");
+    scanf("%d",&n);
+    fact_count=isPrime(n);
+    if(fact_count==2){
+        printf("%d is a prime number\n",n);
+    }
+    else{
+        printf("%d is not a prime number\n",n);
+    }
+    return 0;
+}
