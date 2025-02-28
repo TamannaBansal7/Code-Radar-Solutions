9705b4e6ecd92d9653f54eeda2bfeb37fe4a5a17
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-02-28 14:46:37 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",arr[i]);
-    }
-    bubbleSort(arr,n);
-    printArray(arr,n);
-    return 0;
-}
+def bubble_sort(arr):
+  n = len(arr)
+  for i in range(n):
+    # Flag to optimize, if no swaps occur, the list is already sorted
+    swapped = False
+    for j in range(0, n - i - 1):
+      # Compare adjacent elements
+      if arr[j] > arr[j + 1]:
+        # Swap if they are in the wrong order
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        swapped = True
+    # If no two elements were swapped in inner loop, the array is sorted
+    if not swapped:
+      break
+  return arr
+
+# Example usage
+my_list = [64, 34, 25, 12, 22, 11, 90]
+sorted_list = bubble_sort(my_list)
+print("%d", sorted_list)
