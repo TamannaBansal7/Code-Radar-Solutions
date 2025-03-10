0d8c010b734e47cd7f35c2552c7fcbdd907f4a23
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-03-10 19:00:36 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -1,34 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// Define the structure for a student
-struct Student {
-    int rollNumber;
-    char name[50]; // Assuming a maximum name length of 50 characters
-    float marks;
+#include<stdio.h>
+int main()
+{
+    struct student{
+    int rollno;
+    char name[50];
+    float attendence;
 };
-
-int main() {
-    int n, i;
-
-    // Get the number of students from the user
-    scanf("%d", &n);
-
-    // Dynamically allocate memory for the student array
-    struct Student* students = (struct Student*)malloc(n * sizeof(struct Student));
-
-    // Input student details
-    for (i = 0; i < n; i++) {
-        scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);
-    }
-
-    // Display student details
-    for (i = 0; i < n; i++) {
-        printf("Roll Number: %d, Name: %s, Marks: %.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
-    }
-
-    // Free the dynamically allocated memory
-    free(students);
-
-    return 0;
+struct student stud[1000];
+int n;
+scanf("%d",&n);
+for(int i=0;i<n;i++){
+    scanf("%d",&stud[i].rollno);
+    scanf("%s",&stud[i].name);
+    scanf("%f",&stud[i].attendence);
+}
+for(int i=0;i<n;i++){
+    printf("Roll Number: %d, ",stud[i].rollno);
+    printf("Name: %s, ",stud[i].name);
+    printf("Marks: %.2f\n",stud[i].attendence);
+}
+return 0;
 }
