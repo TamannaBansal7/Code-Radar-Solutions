a76721df0a4c703299b55dc043c265d6af573b63
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-03-27 21:07:43 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,26 +1,36 @@
-#include<stdio.h>
-void bubbleSort(int n,int arr[]){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+#include <stdio.h>
+
+void bubbleSort(int n, int arr[]) {
+    for (int i = 0; i < n - 1; i++) { // Corrected loop condition
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int printArray(int n,int arr[n]){
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d ",&arr[i]);
+int printArray() { // Removed n parameter
+    int n;
+    scanf("%d", &n);
+    int arr[n]; // VLA, works in C99 and later
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]); // Removed space
     }
+
     bubbleSort(n, arr);
-    for(int i=0;i<n;i++){
-        printf("%d",arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]); // Added space
     }
+    printf("\n"); //Added newline for readability.
     return 0;
 }
 
-
+int main() {
+    printArray();
+    return 0;
+}
