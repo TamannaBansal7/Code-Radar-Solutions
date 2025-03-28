36ab988b79da966b7ab1b5458887b1a000ed5fc5
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-03-28 16:58:19 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,8 +1,4 @@
-// Your code here...
-#include <stdio.h>
-#include <stdlib.h>
-
-int smallestWindowToSort(int arr[], int n) {
+int findUnsortedSubarray(int arr[], int n) {
     int left = -1, right = -1;
     int max = arr[0];
     int min = arr[n - 1];
@@ -33,22 +29,4 @@ int smallestWindowToSort(int arr[], int n) {
     return right - left + 1;
 }
 
-int abc() {
-    int t;
-    scanf("%d", &t);
 
-    while (t--) {
-        int n;
-        scanf("%d", &n);
-
-        int *arr = (int *)malloc(n * sizeof(int));
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &arr[i]);
-        }
-
-        printf("%d\n", smallestWindowToSort(arr, n));
-        free(arr);
-    }
-
-    return 0;
-}
