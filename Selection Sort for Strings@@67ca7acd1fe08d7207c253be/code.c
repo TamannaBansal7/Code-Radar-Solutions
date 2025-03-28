e98f002772e385844030c21e77546be1a2822ba9
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-03-29 02:12:12 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to swap two strings
 void swap(char *str1, char *str2) {
-    char temp[100]; // Assuming maximum string length is 100
+    char temp[100]; 
     strcpy(temp, str1);
     strcpy(str1, str2);
     strcpy(str2, temp);
 }
-
-// Function to perform Selection Sort on an array of strings
 void selectionSort(char arr[][100], int n) {
     int i, j, min_idx;
-
-    // One by one move boundary of unsorted subarray
     for (i = 0; i < n - 1; i++) {
-        // Find the minimum element in unsorted array
         min_idx = i;
         for (j = i + 1; j < n; j++) {
-            // Compare strings using strcmp
             if (strcmp(arr[j], arr[min_idx]) < 0) {
                 min_idx = j;
             }
         }
-
-        // Swap the found minimum element with the first element
         swap(arr[min_idx], arr[i]);
     }
 }
