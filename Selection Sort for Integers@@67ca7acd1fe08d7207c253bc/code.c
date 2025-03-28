6e8c29873f0d90124c4364d7a694d8f94605de54
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-03-28 18:41:53 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,42 +1,25 @@
-#include <stdio.h>
-#include <limits.h>
-
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int arr[100];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
-
-
-
+#include<stdio.h>
+#include<limits.h>
+void selectionSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
         int min=INT_MAX;
         int minidx=-1;
-        for(int j=i;j<=n-1;j++){
+        for(j=i;j<n-1;j++){
             if(min>arr[j]){
                 min=arr[j];
                 minidx=j;
-
             }
         }
-
-    int temp=arr[minidx];
-    arr[minidx]=arr[i];
-    arr[i]=temp;
-
-
-    }
-    printf("\n");
-     for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
+        int temp=arr[minidx];
+        arr[minidx]=arr[i];
+        arr[i]=temp;
     }
 
+}
 
-    return 0;
+void printArray(int arr[],int n){
+    selectionSort(arr,n);
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
 }
