a05f8dfc2e3e15a31c7ec3623087041cd549973c
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-04-01 13:09:10 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -2,36 +2,17 @@
 #include<stdio.h>
 #include<stdbool.h>
 int monotonic(int n,int arr[]){
-    int result;
-    for(int i=0;i<n-1;i++){
-        bool flag=true;
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-                flag=false;
-            }
-        }
-        if(flag==true){
-            result=1;
-        }
-    }
+    bool increasing=true;
+    bool decreasing=true;
     for(int i=0;i<n;i++){
-        bool flag=true;
-        for(int j=0;j>n-i-1;j++){
-            if(arr[j]<arr[j+1]){
-                int temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
-                flag=false;
-            }
+        if(arr[i]>arr[i+1]){
+            increasing=false;
         }
-        if(flag==true){
-            result=2;
+        if(arr[i]<arr[i+1]){
+            decreasing=false;
         }
     }
-    return result;
+    return increasing || decreasing;
 }
 
 int main(){
@@ -42,12 +23,12 @@ int main(){
     for(int i=0;i<n;i++){
     scanf("%d",&arr[i]);
     }
-    monotonic(n,arr);
-    if(result==1 || result==2){
+    if(monotonic(n,arr)){
         printf("YES");
     }
     else{
         printf("NO");
     }
+    
     return 0;
 }
