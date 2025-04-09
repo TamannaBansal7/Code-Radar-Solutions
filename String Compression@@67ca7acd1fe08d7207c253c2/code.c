2ec4250c23eaa5eb6d10ba24a3b78f005340b099
TamannaBansal7 <tamanna1821.becse24@chitkara.edu.in>
2025-04-10 01:07:39 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -1,7 +1,7 @@
 // Your code here...
 #include<stdio.h>
 #include<string.h>
-char compressString(char str[100],char compressed){
+void compressString(char str[100],char compressed){
     int count=0;
     for(int i=0;i<strlen(str);i++){
         count++;
